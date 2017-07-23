959dacd6cdd268c46f549daeaa1245c279a45f8d
Netrhil <saragar.jim@gmail.com>
2017-07-23 00:34:31 -0400

    Amononado login
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  layout "paginas_externas"
+
   include Clearance::Controller
+  
   protect_from_forgery with: :exception
 end
--- app/controllers/bienvenida_controller.rb
+++ app/controllers/bienvenida_controller.rb
@@ -1,4 +1,5 @@
 class BienvenidaController < ApplicationController
+  layout "application"
 
   #Solo permite el acceso cuando el usuario se halla logueado exitosamente
   before_action :require_login

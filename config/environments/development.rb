0b679e698764231c684ad5bd938564fff4288e8f
Netrhil <saragar.jim@gmail.com>
2017-07-22 21:29:14 -0400

    Sistema de cambio de contraseña por mail funcionando
    Signed-off-by: Netrhil <saragar.jim@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,26 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  # Escogue que url ocupara para el mailer segun el ambiente
+  if Rails.env.development? || Rails.env.test?
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  elsif Rails.env.production?
+    config.action_mailer.default_url_options = {:host => ENV['HOST']}
+  end
+
+  # Configuracion para smtp con cuenta gmail
+   config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "mail.proyecto.sia@gmail.com",
+    :password => "mailsia123",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+    }
 
 end
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,5 @@
 Clearance.configure do |config|
   config.routes = false
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = "saragar.jim@gmail.com"
   config.rotate_csrf_on_sign_in = true
 end
